2db2c2d82256a74087a101b6238e495584ca1093
793207918@qq.com <793207918@qq.com>
2018-05-04 21:54:44 +0800

    20180504

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
-var gulp = require('gulp');
-var jade = require('gulp-jade');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const production = process.env.NODE_ENV === 'production';
 
 // 打包后更改资源路径为cdn
-// var rev = require('gulp-rev');
-// var revCollector = require('gulp-rev-collector');
-
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var production = process.env.NODE_ENV === 'production';
+const path = require('path');
+const fs = require('fs');
+const revCollector = require('gulp-rev-collector');
 
 gulp.task('html', function() {
     gulp.src('./src/*.jade')
-        .pipe(jade({pretty: production ? false : '\t'}))
+        .pipe(jade({pretty: '\t'}))
         .pipe(gulp.dest('./docs/'))
 });
 
 gulp.task('sass', function() {
     gulp.src('./src/scss/*.scss')
         .pipe(sass({
-            outputStyle: production ? 'compressed' : 'expanded',
-            indentWidth: production ? 0 : 4
+            outputStyle: 'expanded',
+            indentWidth: 4
         }))
         .pipe(autoprefixer({browsers: ['last 50 versions']}))
         .pipe(gulp.dest('./docs/css/'));
@@ -60,12 +62,63 @@ gulp.task('auto', function() {
     gulp.watch('./docs/**/*.*').on('change', browserSync.reload);
 });
 
+gulp.task('writeRevManifest', function() {
+    writeRevManifest();
+});
 
-// gulp.task('rev', function () {
-//
-// });
+gulp.task('revHtml', function () {
+    gulp.src(['./rev-manifest.json', './docs/*.html'])
+        .pipe(revCollector({
+            replaceReved: true,
+            dirReplacements: {
+                './css': 'http://static.67one.com/css/',
+                './js': 'http://static.67one.com/js/',
+                './img': 'http://static.67one.com/img/',
+            }
+        }))
+        .pipe(htmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            removeEmptyAttributes: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
+        .pipe(gulp.dest('./docs/'));
+});
+
+gulp.task('revCss', function () {
+    gulp.src(['./rev-manifest.json', './docs/css/*.css'])
+        .pipe(revCollector({
+            replaceReved: true,
+            dirReplacements: {
+                '../img': 'http://static.67one.com/img/'
+            }
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./docs/css/'));
+});
+
 
 gulp.task('default', ['html', 'css', 'sass', 'script', 'image', 'server', 'auto']);
-gulp.task('build', ['html', 'css', 'sass', 'script', 'image']);
+gulp.task('build', ['html', 'css', 'sass', 'script', 'image', 'writeRevManifest', 'revHtml', 'revCss']);
 
-// gulp.task('default', ['rev']);
+
+function writeRevManifest() {
+    let docsPath = path.resolve(__dirname, './docs/');
+    let docs = fs.readdirSync(docsPath);
+    let files = {};
+
+    docs.forEach(item => {
+        let fileDirPath = docsPath + '\\' + item;
+        let stat = fs.statSync(fileDirPath)
+        if(stat.isDirectory()){
+            let filesList = fs.readdirSync(fileDirPath);
+            filesList.forEach(file => {
+                files[file] = file;
+            })
+        }
+    });
+
+    fs.writeFileSync('rev-manifest.json', JSON.stringify(files))
+}
