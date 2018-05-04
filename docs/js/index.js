3cf6d2da023f98d67bbf5a130a18efdd7f3fcc95
793207918@qq.com <793207918@qq.com>
2018-05-04 22:45:23 +0800

    20180504

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,11 +1,1 @@
-window.onload = function() {
-    console.log('Welcome 67one');
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        autoplay:true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable :true
-        }
-    });
-};
+window.onload=function(){console.log("Welcome 67one");new Swiper(".swiper-container",{loop:!0,autoplay:!0,pagination:{el:".swiper-pagination",clickable:!0}})};
